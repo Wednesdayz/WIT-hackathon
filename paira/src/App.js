259f4bfec328a9b63aa649687b385cfaaaea1ddf
fuzzywuzzy21 <fuzzywuzzy21>
2020-07-11 13:15:41 +1000

    added homePage.js

--- paira/src/App.js
+++ paira/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import homePage from './components/homepage/homePage';
+import { BrowserRouter ,Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="main-content">
+        <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={homePage} />
+        </Switch>
+        </BrowserRouter>
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ paira/src/components/homepage/homePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homePage.css';
+
+const HomePage = () =>{
+    return(
+        <div>
+            Koala"s are cute little creatures
+        </div>
+    );
+    }
+
+
+export default HomePage;
